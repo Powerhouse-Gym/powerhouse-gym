087677c884e37b8e7130a9c0ab1f61d919a05312
Zak Ward <zakward85@gmail.com>
2024-10-07 12:45:22 -0500

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from "react-router-dom"
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+import logo from "../assets/logo-white.webp"
 
 function Navbar(props) {
 
@@ -15,6 +16,15 @@ function Navbar(props) {
 
 
     return (
+        <>
+        <div style = {{border: "1px solid black", display: "flex"}}>
+            <img src = {logo} id = "logo"/>
+            <div className = "contact-banner">
+                <p>501-247-1517</p>
+                <p>Facebook</p>
+                <p>Instagram</p>
+            </div>
+        </div>
         <Box className="nav-container">
             <Tabs value={window.location.pathname} >
                 <Tab className = "nav-item" label="Home" value="/" onClick={() => handleNavigate("/")}  />
@@ -24,6 +34,7 @@ function Navbar(props) {
                 <Tab label="Meet the Team" value="/team" onClick={() => handleNavigate("/team")} />
             </Tabs>
         </Box>
+        </>
     );
 }
 
