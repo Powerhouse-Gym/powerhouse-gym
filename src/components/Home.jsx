52eaefa6341378ae151dd862248755d99d688d00
Zak Ward <zakward85@gmail.com>
2024-10-07 15:25:38 -0500

    map

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
+
 
 function Home() {
+const marker = {lat:34.9749, lng: -92.0163}
     return (
-        <>
-        Home
-        </>
-     );
+        <div id ="home">
+            <h1>Come see us!</h1>
+            <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+                <Map
+                    style={{ width: '75vw', height: '75vh' }}
+                    defaultCenter= {{lat: 34.94580, lng: -92.00977}}
+                    defaultZoom={17}
+                    gestureHandling={'greedy'}
+                    disableDefaultUI={true}
+                />
+            </APIProvider>
+        </div>
+    );
 }
 
 export default Home;
