bd2806c0f00e93d1ad1cd2f643af527891f7685f
Zak Ward <zakward85@gmail.com>
2024-10-07 17:15:42 -0500

    map fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,28 @@
-import React, {useEffect} from 'react';
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
+import React, { useEffect } from 'react';
+import { APIProvider, Map, AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
+import athlete from "../assets/athlete.png"
 
 
 function Home() {
-const marker = {lat:34.9749, lng: -92.0163}
+    
+
     return (
-        <div id ="home">
+        <div id="home">
             <h1>Come see us!</h1>
             <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
                 <Map
-                    style={{ width: '55vw', height: '55vh', border: "4px solid black" }}
-                    defaultCenter= {{lat: 34.94580, lng: -92.00977}}
+                    style={{ width: '55vw', height: '55vh', border: "4px solid black", borderRadius: "8px" }}
+                    defaultCenter={{ lat: 34.94580, lng: -92.00977 }}
                     defaultZoom={17}
                     gestureHandling={'greedy'}
                     disableDefaultUI={true}
+                    mapId={import.meta.env.VITE_MAP_ID}
                 />
+                <AdvancedMarker position={{ lat: 34.94580, lng: -92.00977 }} title="PowerHouse Crossfit and Training"  >
+                    <Pin background={'#ed1c26'} glyphColor={'#000'} borderColor={'#000'} scale={2.0} glyph={"PH"} />
+                </AdvancedMarker>
             </APIProvider>
+            <a href="https://www.flaticon.com/free-icons/gym" title="gym icons">Gym icons created by Freepik - Flaticon</a>
         </div>
     );
 }
