128459204279e20f2dde0098993a704144870f9e
Jordan Burger <jordanburger22@gmail.com>
2024-10-08 21:19:56 +0000

    fixed width for home page

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -4,16 +4,18 @@ import turfpic from "../assets/turfpic.webp"
 
 
 function Carousel() {
-    return ( 
-        <div style = {{backgroundImage: `url(${sugarskull})`, height: "500px", width: "100vw",  display: "flex", justifyContent: "center", alignItems: "center", backgroundSize: "130%", backgroundPositionY: "50%", backgroundPositionX: "50%"} }>
+    return (
+        <div style={{ backgroundImage: `url(${sugarskull})`, height: "500px", width: "100%", display: "flex", justifyContent: "center", alignItems: "center", backgroundSize: "130%", backgroundPositionY: "50%", backgroundPositionX: "50%" }}>
 
-        <div style = {{height: "400px", width: "50vw", border: "1px solid grey", backgroundColor: "rgba(0, 0, 0, 0.5)" /* Black with 50% transparency */
-, display: "flex", flexDirection: "column", gap: "25px", alignItems: "center", justifyContent: "center"} }>
-            <h1 style = {{color: "white"}}>Batting Cages</h1>
-            <img src = {turfpic} style = {{border: "1px solid black", height: "75%", width: "75%"}}/>
+            <div style={{
+                height: "400px", width: "50vw", border: "1px solid grey", backgroundColor: "rgba(0, 0, 0, 0.5)" /* Black with 50% transparency */
+                , display: "flex", flexDirection: "column", gap: "25px", alignItems: "center", justifyContent: "center"
+            }}>
+                <h1 style={{ color: "white" }}>Batting Cages</h1>
+                <img src={turfpic} style={{ border: "1px solid black", height: "75%", width: "75%" }} />
+            </div>
         </div>
-        </div>
-     );
+    );
 }
 
 export default Carousel;
