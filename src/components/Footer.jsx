8267b6805bbb4bb3c8b6f91342874b6829a5498b
Zak Ward <zakward85@gmail.com>
2024-10-07 22:05:39 -0500

    footerlogosagain

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from "../assets/powerhouse-grafiti.png"
+import logoActual from "../assets/logo-white.webp"
 
 const Footer = () => {
 
@@ -22,11 +23,12 @@ const footerStyle = {
 
   return (
     <div style={footerStyle}>
+    <img className = "footer-logo" src= {logoActual} alt="powerhouse logo"/>
       <p>PowerHouse Crossfit and Training Complex</p>
       <p>1804 S. Pine Street, Cabot AR 72023</p>
-    <img className = "footer-logo" src= {logo} alt="powerhouse logo" />
       <p>501.247.1517</p>
       <p>Powerhouse Training Complex, LLC</p>
+    <img className = "footer-logo" src= {logo} alt="powerhouse logo" />
     </div>
   );
 };
