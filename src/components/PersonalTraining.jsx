b8390c24f9a2ed0d5e89e3b01a07be5490cc8f07
Zak Ward <zakward85@gmail.com>
2024-11-13 10:58:34 -0600

    zpush

--- src/components/PersonalTraining.jsx
+++ src/components/PersonalTraining.jsx
@@ -3,14 +3,13 @@ import { cageAndFieldRentals } from "../training-data";
 import { useNavigate } from "react-router-dom";
 import PTimg from "../assets/crossfit/personaltraining.jpg";
 import baseballimg from "../assets/baseballbackground.jpg";
-import adaptiveImg from "../assets/adaptive-img.jpg"
-import deadlift from "../assets/deadlift.jpg"
-import teampic from "../assets/teampic.jpg"
-import grouppic from "../assets/groupic2.jpg"
-import oneOnone from "../assets/101.jpg"
-import ptPic from "../assets/pt-pic.jpg"
-import rowerpic from "../assets/rowerpic.jpg"
-
+import adaptiveImg from "../assets/adaptive-img.jpg";
+import deadlift from "../assets/deadlift.jpg";
+import teampic from "../assets/teampic.jpg";
+import grouppic from "../assets/groupic2.jpg";
+import oneOnone from "../assets/101.jpg";
+import ptPic from "../assets/pt-pic.jpg";
+import rowerpic from "../assets/rowerpic.jpg";
 
 const PersonalTraining = () => {
   const teamTraining = cageAndFieldRentals.find(
@@ -21,7 +20,10 @@ const PersonalTraining = () => {
 
   return (
     <>
-      <div className="pt-wrapper" style={{ backgroundImage: `url(${rowerpic})` }}>
+      <div
+        className="pt-wrapper"
+        style={{ backgroundImage: `url(${rowerpic})` }}
+      >
         <div className="intro-section">
           <h1>Powerhouse PERSONAL Training</h1>
           <p>
@@ -35,22 +37,27 @@ const PersonalTraining = () => {
       <h1
         className="pt-service-header"
         style={{
-            display: "flex",
-            fontSize: "46px",
-            height: "8vh",
-            paddingTop: "50px",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
+          display: "flex",
+          fontSize: "46px",
+          height: "8vh",
+          paddingTop: "50px",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
         Services
       </h1>
       <div className="personal-training-container">
-       
         <div className="service-section">
-            <img src= {adaptiveImg} alt="adaptive training" />
-          <h2 style={{ textDecoration: "underline", fontStyle: "italic", textAlign: "center" }}>
+          <img src={adaptiveImg} alt="adaptive training" />
+          <h2
+            style={{
+              textDecoration: "underline",
+              fontStyle: "italic",
+              textAlign: "center",
+            }}
+          >
             Now Offering!
           </h2>
           <h3>Inclusive & Adaptive Training</h3>
@@ -58,36 +65,44 @@ const PersonalTraining = () => {
             Specialized training designed to be adaptive and inclusive for all
             abilities.
           </p>
-          <a
-            href="tel:+15012471517"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button>Call for more information</button>
+          <a href="tel:+15012471517" target="_blank" rel="noopener noreferrer">
+            <button>
+              <strong>CALL</strong> for more information
+            </button>{" "}
           </a>
         </div>
-          <div className="service-section">
-          <img src= {deadlift} alt="deadlift training" />
-            <h3>Sports-Specific Strength & Conditioning</h3>
+        <div className="service-section">
+          <img src={deadlift} alt="deadlift training" />
+          <h3>Sports-Specific Strength & Conditioning</h3>
 
-            <p>
-              Customized training focused on building sport-specific strength
-              and agility.
-            </p>
-            <button onClick={() => navigate("/training")}>More Info</button>
-          </div>
+          <p>
+            Customized training focused on building sport-specific strength and
+            agility.
+          </p>
+          <button onClick={() => navigate("/training")}>
+            <strong>CLICK</strong> for more information
+          </button>
+        </div>
 
-          <div className="service-section">
-            <img src = {teampic} alt = "group" />
-            <h3>Team Strength & Conditioning</h3>
-            <p>
-              Strength and conditioning sessions for teams (up to 12 athletes).
-            </p>
-            <p>
-              <strong>$180 per session</strong>
-            </p>
-            <div style = {{paddingBottom: "15px", display: "flex", overflowX: "auto", whiteSpace: "nowrap", width : "100%"}} className="button-scroll"> 
-
+        <div className="service-section">
+          <img src={teampic} alt="group" />
+          <h3>Team Strength & Conditioning</h3>
+          <p>
+            Strength and conditioning sessions for teams (up to 12 athletes).
+          </p>
+          <p>
+            <strong>$180 per session</strong>
+          </p>
+          <div
+            style={{
+              paddingBottom: "15px",
+              display: "flex",
+              overflowX: "auto",
+              whiteSpace: "nowrap",
+              width: "100%",
+            }}
+            className="button-scroll"
+          >
             <a href={teamTraining.url1}>
               <button>{teamTraining.urlTitle1}</button>
             </a>
@@ -100,47 +115,43 @@ const PersonalTraining = () => {
             <a href={teamTraining.url4}>
               <button>{teamTraining.urlTitle4}</button>
             </a>
-            </div>
           </div>
+        </div>
 
-          <div className="service-section">
-            <img src = {grouppic} alt = "group pic" />
-            <h3>Small Group Training</h3>
-            <p>
-              2-on-1 Training: <strong>$90 per hour</strong>
-            </p>
-            <p>
-              4-on-1 Training: <strong>$150 per hour</strong>
-            </p>
-            <a
-              href="tel:+15012471517"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button>Call for more information</button>
-            </a>
-          </div>
-
-          <div className="service-section">
-            <img src = {oneOnone} alt = "crossfit class"/>
-            <h3>Crossfit Onboarding</h3>
-            <p>
-              Four-week program, including two sessions per week and one
-              CrossFit class each week.
-            </p>
-            <p>
-              <strong>$250</strong>
-            </p>
-            <a
-              href="tel:+15012471517"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button>Call for more information</button>
-            </a>
-          </div>
         <div className="service-section">
-          <img src = {ptPic} alt ="pt picture" />
+          <img src={grouppic} alt="group pic" />
+          <h3>Small Group Training</h3>
+          <p>
+            2-on-1 Training: <strong>$90 per hour</strong>
+          </p>
+          <p>
+            4-on-1 Training: <strong>$150 per hour</strong>
+          </p>
+          <a href="tel:+15012471517" target="_blank" rel="noopener noreferrer">
+            <button>
+              <strong>CALL</strong> for more information
+            </button>
+          </a>
+        </div>
+
+        <div className="service-section">
+          <img src={oneOnone} alt="crossfit class" />
+          <h3>Crossfit Onboarding</h3>
+          <p>
+            Four-week program, including two sessions per week and one CrossFit
+            class each week.
+          </p>
+          <p>
+            <strong>$250</strong>
+          </p>
+          <a href="tel:+15012471517" target="_blank" rel="noopener noreferrer">
+            <button>
+              <strong>CALL</strong> for more information
+            </button>
+          </a>
+        </div>
+        <div className="service-section">
+          <img src={ptPic} alt="pt picture" />
           <h3 style={{ textDecoration: "underline", fontStyle: "italic" }}>
             Coming Soon in 2025
           </h3>
@@ -149,23 +160,21 @@ const PersonalTraining = () => {
           <p>
             <strong>$70 per hour</strong>
           </p>
-          <a
-            href="tel:+15012471517"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button>Call for more information</button>
+          <a href="tel:+15012471517" target="_blank" rel="noopener noreferrer">
+            <button>
+              <strong>CALL</strong> for more information
+            </button>
           </a>
         </div>
-        </div>
-        {/* <Divider
+      </div>
+      {/* <Divider
           orientation="vertical"
           flexItem
           sx={{ backgroundColor: "white" }}
         /> */}
-        {/* <div className="personal-training-section pt-left">
+      {/* <div className="personal-training-section pt-left">
         </div> */}
-     
+
       {/* <Divider sx={{ backgroundColor: "white" }} /> */}
     </>
   );
